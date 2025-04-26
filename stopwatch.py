15816252e85befcaf4e256705997e43820eab429
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2025-04-26 22:18:29 +0200

    Add TimeDisplay Class

--- stopwatch.py
+++ stopwatch.py
@@ -1,15 +1,39 @@
+from time import monotonic
+
 from textual.app import App, ComposeResult
 from textual.widgets import Button, Digits, Footer, Header
 from textual.containers import HorizontalGroup, VerticalScroll
+from textual.reactive import reactive
 
 
 class TimeDisplay(Digits):
     """A widget to display elapsed time."""
-    pass
+    start_time = reactive(monotonic)
+    time = reactive(0.0)
 
+    def on_mount(self) -> None:
+        """Event handler called when widget is added to the app."""
+        self.set_interval(1/60, self.update_time)
+
+    def update_time(self) -> None:
+        """Method to update the time to the current time."""
+        self.time = monotonic() - self.start_time
+    
+    def watch_time(self, time: float) -> None:
+        """Called when the time attribute changes."""
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02,.0f}:{seconds:05,.0f}")
 
 class Stopwatch(HorizontalGroup):
     """A stopwatch widget."""
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        if event.button.id == "start":
+            self.add_class("started")
+        elif event.button.id == "stop":
+            self.remove_class("started")
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the stopwatch."""
@@ -30,7 +54,7 @@ class StopwatchApp(App):
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Header(icon="A", show_clock=True)
+        yield Header(icon="", show_clock=True)
         yield Footer()
         yield VerticalScroll(Stopwatch(), Stopwatch(), Stopwatch())
 
